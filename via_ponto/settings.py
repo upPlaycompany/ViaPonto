6f07307feae6f732eb69f3fad73c3a8de83ece88
deodatojunior <deodatojunior999@gmail.com>
2021-04-24 11:49:11 -0400

    A

--- via_ponto/settings.py
+++ via_ponto/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    BASE_DIR, 'via_ponto/static'
+    BASE_DIR, 'static'
 ]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
