07521100b75f2a5464befd519a1afd472b2cdccf
deodatojunior <deodatojunior999@gmail.com>
2021-04-24 11:50:28 -0400

    A

--- via_ponto/settings.py
+++ via_ponto/settings.py
@@ -123,9 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'via_ponto/static')
 STATICFILES_DIRS = [
-    BASE_DIR, 'static'
+    BASE_DIR, 'via_ponto/static'
 ]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
