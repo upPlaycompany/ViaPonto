dd08431a1ef4939ff46ad90af642e17f873db83f
DieissonNarde <dieissonnarde@gmail.com>
2021-06-12 11:57:57 -0400

    :sparkles:  Add cargo edit

--- via_ponto/views.py
+++ via_ponto/views.py
@@ -781,6 +781,51 @@ def cadastro_cargo(request, token):
     return render(request, 'cadastro_cargo.html', {'lista': key})
 
 
+def edit_cargo(request, token, id):
+    conexao = requests.api.request('GET', 'https://parseapi.back4app.com/users/me',
+                                    headers={"X-Parse-Application-Id": "Sgx1E183pBATq8APs006w2ACmAPqpkk33jJwRGC6",
+                                        "X-Parse-REST-API-Key": "lA1fgtFCTA2A5o0ebhuQM8T7DSAErYCPMF4jQtp9",
+                                        "X-Parse-Session-Token": f"{token}"})
+    response = conexao.json()
+    if str(response['sessionToken']) != f"{token}":
+        return redirect('login')
+    elif response['empresa_confirmacao'] == False:
+        return redirect('login')
+    else:
+        pass
+    key = [{'id': token, 'user': response['username']}]
+    empresa_id = response['id_empresa']['objectId']
+
+    req_carg = requests.api.request('GET', f"https://parseapi.back4app.com/classes/Cargo/{id}",
+                                    headers={
+                                        "X-Parse-Application-Id": "Sgx1E183pBATq8APs006w2ACmAPqpkk33jJwRGC6",
+                                        "X-Parse-REST-API-Key": "lA1fgtFCTA2A5o0ebhuQM8T7DSAErYCPMF4jQtp9"})
+    carg = req_carg.json()
+
+    if request.method == 'POST':
+        nome = request.POST['nome_cargo']
+
+        req_dep = requests.api.request('PUT', f"https://parseapi.back4app.com/classes/Cargo/{id}",
+                                        headers={
+                                            "X-Parse-Application-Id": "Sgx1E183pBATq8APs006w2ACmAPqpkk33jJwRGC6",
+                                            "X-Parse-REST-API-Key": "lA1fgtFCTA2A5o0ebhuQM8T7DSAErYCPMF4jQtp9",
+                                            "Content-Type": "application/json"},
+                                        json={
+                                            "nome": f"{nome}",
+                                            "id_empresa": {
+                                                '__type': "Pointer",
+                                                "className": "Empresa",
+                                                "objectId": empresa_id
+                                            }})
+        status = str(req_dep.status_code)
+        if status == '200':
+            return redirect('list_cargo', token=token)
+        else:
+            return redirect('fail_default', token=token)
+
+    return render(request, 'edit_cargo.html', {'lista': key, 'cargo': carg})
+
+
 def list_colaborador(request, token):
     conexao = requests.api.request('GET', 'https://parseapi.back4app.com/users/me',
                                     headers={"X-Parse-Application-Id": "Sgx1E183pBATq8APs006w2ACmAPqpkk33jJwRGC6",
