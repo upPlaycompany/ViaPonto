a4c971d319dd5f83d5a14927aca8b03f69b0d7d0
DieissonNarde <dieissonnarde@gmail.com>
2021-07-28 10:11:15 -0400

    :bug:  Fix error static css in settings.py

--- via_ponto/settings.py
+++ via_ponto/settings.py
@@ -119,9 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'via_ponto/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    BASE_DIR, 'via_ponto/static'
+    BASE_DIR, 'static'
 ]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
